eacda979ee259b1789c18b7261cd81372134fc4b
Kaiky Victório <kaikyvictorio2006@gmail.com>
2024-07-22 20:36:15 -0300

    aula01GIT

---
+++ kaiky-1.0.js
@@ -0,0 +1,12 @@
+let fortal= Number(prompt("VOCÊ TORCE POR FORTALEZA? /n 1. SIM /n 2.NAO"))?.toLowerCase
+let fortal1= String(fortal)
+switch (fortal1) {
+    case 1:
+        alert("BEM VINDO A SELEÇÃO")
+        break;
+
+    case 2:
+        alert("SAIA DAQUI")
+        break;
+
+}
